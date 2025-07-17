d0343acd58c17dda559245bfe75b5719f9f5818c
rsingh-canopyhealth <ranjit.singh@canopyhealth.com>
2025-07-17 16:08:03 -0700

    removed name

--- dbo/Tables/RANJIT_TABLE.sql
+++ dbo/Tables/RANJIT_TABLE.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[RANJIT_TABLE] (
-    [ID]    INT          NULL,
-    [NAME]  VARCHAR (20) NULL,
-    [STATE] VARCHAR (1)  NULL
+    [ID]    INT         NULL,
+    [STATE] VARCHAR (1) NULL
 );
 
